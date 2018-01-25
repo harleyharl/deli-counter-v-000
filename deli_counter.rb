83e090650b9f685b37f162e634c544ac16cad47f
Harley Rayner <harleymteden@gmail.com>
2018-01-25 19:17:20 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ def line(people_in_line)
     puts "The line is currently empty."
   elsif people_in_line.size > 0
     counter = 0
-    puts "The line is currently: #{counter + 1}.#{people_in_line[counter]}"
+    puts "The line is currently: #{counter + 1}. #{people_in_line[counter]}"
     counter += 1
   end
 end
