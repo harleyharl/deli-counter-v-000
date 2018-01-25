85e954c559e9c4a1c599103936c723d31bf6d2a9
Harley Rayner <harleymteden@gmail.com>
2018-01-25 19:55:08 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -19,10 +19,9 @@ def line(people_in_line)
   if people_in_line.size == 0
     puts "The line is currently empty."
   elsif people_in_line.size > 0
-    index_and_name = []
+    line_display = []
     people_in_line.each do |index|
-      index_and_name << ["#{index + 1}. #{people_in_line[index]}"]
-      index += 1
+      line_display << ["#{index + 1}. #{people_in_line[index]}"]
 
       # puts "The line is currently: #{people_in_line}. #{people_in_line[counter]}"
 
